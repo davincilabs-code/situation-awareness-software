ebadef513549548f4c8801d7c0455b3735f505f8
tony4979 <tony4979@naver.com>
2024-12-04 08:06:09 +0900

    remove comment

--- test/test_bench/export.py
+++ test/test_bench/export.py
@@ -1,13 +1,4 @@
-
-# def load_models(model_path):
-#     """TensorFlow Lite와 ONNX 모델을 로드합니다."""
-#     # TensorFlow Lite 모델 로드
-#     tf_lite_model = YOLO(model_path)
-#     tf_lite_model.export(format='tflite')
-#     tflite_path = str(Path(model_path).with_suffix('.tflite'))
-#     tf_lite_interpreter = tf.lite.Interpreter(model_path=tflite_path)
-#     tf_lite_interpreter.allocate_tensors()
-    
+   
 #     # ONNX 모델 로드
 #     onnx_path = str(Path(model_path).with_suffix('.onnx'))
 #     YOLO(model_path).export(format='onnx')
