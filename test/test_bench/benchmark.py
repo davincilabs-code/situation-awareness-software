1c61578fe9c38a1e5f0a8cf4303e2a32495f8b3a
tony4979 <tony4979@naver.com>
2024-12-04 08:00:42 +0900

    remove comment

--- test/test_bench/benchmark.py
+++ test/test_bench/benchmark.py
@@ -1,11 +1,4 @@
-#     # 메모리 사용 출력
-#     if tf_lite_16_memories:
-#         print(f"TensorFlow Lite Float16 평균 메모리 사용량: {mean(tf_lite_16_memories[10:]) / 1024:.2f} KB")
-#     if tf_lite_32_memories:
-#         print(f"TensorFlow Lite Float32 평균 메모리 사용량: {mean(tf_lite_32_memories[10:]) / 1024:.2f} KB")
-#     if onnx_memories:
-#         print(f"ONNX 평균 메모리 사용량: {mean(onnx_memories[10:]) / 1024:.2f} KB")
-    
+  
 #     # 벤치마크 그래프 저장
 #     save_benchmark_graphs(tf_lite_16_times, tf_lite_32_times, onnx_times, 
 #                           tf_lite_16_memories, tf_lite_32_memories, onnx_memories)
