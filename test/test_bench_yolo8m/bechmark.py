94c9845c3dc8635736f0aa099a8a2c08d34338a5
tony4979 <tony4979@naver.com>
2024-12-04 08:22:56 +0900

    code

--- test/test_bench_yolo8m/bechmark.py
+++ test/test_bench_yolo8m/bechmark.py
@@ -82,3 +82,4 @@ def benchmark_inference_extended(model_tf_lite_16, model_tf_lite_32, model_onnx,
         input_name = model_onnx.get_inputs()[0].name
         for image_path in tqdm(image_paths, desc="ONNX"):
             image = prepare_image_onnx(image_path)
+            for _ in range(num_iterations):
