fc8da98176730bb8e7ceb313a3f6ebc6591de6ad
tony4979 <tony4979@naver.com>
2024-12-04 08:32:58 +0900

    code

--- test/test_bench_yolo8m/bechmark.py
+++ test/test_bench_yolo8m/bechmark.py
@@ -117,3 +117,5 @@ def save_benchmark_graphs(tf_lite_16_times, tf_lite_32_times, onnx_times,
     plt.title("Benchmark: Average Memory Usage")
     for i, v in enumerate(avg_memories):
         plt.text(i, v + 5, f"{v:.2f} KB", ha='center', fontweight='bold')  # 각 막대 위에 값 라벨 추가
+
+    plt.legend(bars, model_names, loc='center right', title="Model Type")
