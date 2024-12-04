0a993ebd2196c6ba13f561381a5ab58ebaa51a30
tony4979 <tony4979@naver.com>
2024-12-04 09:29:40 +0900

    code

--- test/test_bench_yolo8m/export.py
+++ test/test_bench_yolo8m/export.py
@@ -163,6 +163,7 @@ def load_models(model_path):
 def prepare_image(image_path, input_size=(640, 640)):
     img = Image.open(image_path)
     img = img.resize(input_size)
+    img = np.array(img) / 255.0
 # import time
 # import os
 # import numpy as np
