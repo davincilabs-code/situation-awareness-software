68ea1b0d98758aad160432f06e7220ebbf6ed9c1
tony4979 <tony4979@naver.com>
2024-12-04 09:49:55 +0900

    code

--- test/test_bench_yolo8m/export.py
+++ test/test_bench_yolo8m/export.py
@@ -166,8 +166,6 @@ def prepare_image(image_path, input_size=(640, 640)):
     img = np.array(img) / 255.0
     img = np.expand_dims(img, axis=0).astype(np.float32)
     return img
-
-#         input_name = model_onnx.get_inputs()[0].name
 #         for image_path in tqdm(image_paths, desc="ONNX"):
 #             image = prepare_image(image_path)
 #             for _ in range(num_iterations):
