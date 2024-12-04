6487b537d2d33b73ae0b74f5275d31783d0b36d3
tony4979 <tony4979@naver.com>
2024-12-04 09:30:24 +0900

    code

--- test/test_bench_yolo8m/export.py
+++ test/test_bench_yolo8m/export.py
@@ -170,6 +170,8 @@ def prepare_image(image_path, input_size=(640, 640)):
 def benchmark_inference(model_tf_lite, model_onnx, image_paths, num_iterations=100):
     tf_lite_times = []
     if model_tf_lite:
+        for image_path in tqdm(image_paths, desc="TensorFlow Lite"):
+            image = prepare_image(image_path)
 # import time
 # import os
 # import numpy as np
